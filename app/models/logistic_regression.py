302165af8809d93bbc8afb48c54a56b3e3211c72
maharshi_tecmantras <maharshi.tecmantras@gmail.com>
2026-06-30 15:36:25 +0530

    Initial commit of logistic regression implementation.

---
+++ app/models/logistic_regression.py
@@ -0,0 +1,16 @@
+# logistic_regression.py
+
+# Logistic Regression Implementation
+
+class LogisticRegression:
+    def __init__(self, iterations=1000, learning_rate=0.01):
+        self.iterations = iterations
+        self.learning_rate = learning_rate
+
+    def fit(self, X, y):
+        # Fit the model to the training data
+        pass
+
+    def predict(self, X):
+        # Predict using the logistic regression model
+        pass
